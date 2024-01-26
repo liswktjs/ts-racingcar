b4a7db5b1f69b06044370101cdadb78ea81cb3f6
JASUN LEE <liswktjs@gmail.com>
2024-01-27 01:46:37 +0900

    feat: Input Element 구현

---
+++ src/components/@common/Input/index.ts
@@ -0,0 +1,22 @@
+import appendChildElement from '../../../utils/appendChildElement';
+import './index.css';
+
+interface Props {
+  id: string;
+  inputType: 'text' | 'number';
+  placeholder?: string;
+  parentId: string;
+}
+
+const Input = ({ id, parentId, inputType, placeholder }: Props): void => {
+  const input = document.createElement('input');
+
+  input.placeholder = placeholder ?? '';
+  input.id = id;
+  input.type = inputType;
+  input.classList.add('input');
+
+  appendChildElement({ parentId, targetElement: input });
+};
+
+export default Input;
