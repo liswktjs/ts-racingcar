1faa441c2de7aa18ebadb1e0184881468f6f8563
JASUN LEE <liswktjs@gmail.com>
2024-01-27 00:48:55 +0900

    chore: 초기 설정
    웹팩 ts eslint config 추가

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', 'mjs', 'ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+  devtool: 'source-map',
+  target: 'node',
+  devServer: {
+    static: './dist',
+  },
+};
