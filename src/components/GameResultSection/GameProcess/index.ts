2ef99cf7fcc5e48c6eb587e749d664c2bc6c6efd
un S <liswktjs@gmail.com>
2024-01-29 09:53:39 +0900

    feat: car이름들 렌더링하기

--- src/components/GameResultSection/GameProcess/index.ts
+++ src/components/GameResultSection/GameProcess/index.ts
@@ -5,11 +5,31 @@ interface Props {
   gameCount: number;
 }
 
-const GameProcess = ({ carList, gameCount }: Props): void => {
-  const carGameResult = [];
-  carList.forEach((_, index) => {
-    carGameResult[index] = getGameResult({ count: gameCount });
+const renderRacingGame = ({
+  carList,
+  gameResult,
+}: {
+  carList: string[];
+  gameResult: number[];
+}): void => {
+  const container = document.createElement('div');
+  container.id = 'car-list-container';
+
+  carList.forEach((car, index) => {
+    const element = document.createElement('div');
+    element.innerText = car;
+
+    container.appendChild(element);
   });
+};
+
+const GameProcess = ({ carList, gameCount }: Props): void => {
+  const gameResult: number[] = [];
+  carList.forEach((_, index) => {
+    gameResult[index] = getGameResult({ count: gameCount });
+  });
+
+  renderRacingGame({ carList, gameResult });
 };
 
 export default GameProcess;
