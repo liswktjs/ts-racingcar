b1b21812a8322e1a21cbd197ab116c1ad3b0072c
JASUN LEE <liswktjs@gmail.com>
2024-01-30 01:06:26 +0900

    feat: 랜덤하게 나온 결과로 게임을 진행하여 결과를 보여주는 컴포넌트 구현

--- src/components/GameResultSection/GameProcess/index.ts
+++ src/components/GameResultSection/GameProcess/index.ts
@@ -1,35 +1,83 @@
-import { getGameResult } from '../../../utils';
+import { DOM_ID } from '../../../constant';
+import { appendChildElement, getGameResult } from '../../../utils';
 
 interface Props {
-  carList: string[];
+  carObject: Record<string, number>;
   gameCount: number;
 }
 
-const renderRacingGame = ({
-  carList,
-  gameResult,
-}: {
-  carList: string[];
-  gameResult: number[];
-}): void => {
+const renderRacingGame = (carObject: Record<string, number>): void => {
   const container = document.createElement('div');
   container.id = 'car-list-container';
 
-  carList.forEach((car, index) => {
-    const element = document.createElement('div');
-    element.innerText = car;
+  Object.keys(carObject).forEach((name) => {
+    const container = document.createElement('div');
+    container.classList.add('car-container');
 
-    container.appendChild(element);
+    const nameElement = document.createElement('div');
+    nameElement.innerText = name;
+
+    container.appendChild(nameElement);
+
+    for (let i = 0; i < (carObject[name] ?? 0); i++) {
+      const arrow = document.createElement('div');
+      arrow.innerText = 'V';
+
+      container.appendChild(arrow);
+    }
+
+    appendChildElement({
+      targetElement: container,
+      parentId: DOM_ID.gameResult,
+    });
   });
 };
 
-const GameProcess = ({ carList, gameCount }: Props): void => {
-  const gameResult: number[] = [];
-  carList.forEach((_, index) => {
-    gameResult[index] = getGameResult({ count: gameCount });
-  });
+const renderGameWinner = (carObject: Record<string, number>): void => {
+  const winners = Object.entries(carObject).reduce<{
+    maxValue: number | null;
+    keys: string[];
+  }>(
+    (acc, [key, value]) => {
+      if (acc.maxValue === null) {
+        return { maxValue: value, keys: [key] };
+      }
+      if (value > acc.maxValue) {
+        return { maxValue: value, keys: [key] };
+      }
+      if (value === acc.maxValue) {
+        acc.keys.push(key);
+      }
+      return acc;
+    },
+    { maxValue: null, keys: [] }
+  ).keys;
 
-  renderRacingGame({ carList, gameResult });
+  const winnerElement = document.createElement('h2');
+  winnerElement.innerText = `${winners.join(',')}`;
+  appendChildElement({
+    parentId: DOM_ID.gameResult,
+    targetElement: winnerElement,
+  });
+};
+
+const handleCarGame = ({
+  carObject,
+  gameCount,
+}: {
+  carObject: Record<string, number>;
+  gameCount: number;
+}): void => {
+  Object.keys(carObject).forEach((item) => {
+    carObject[item] = getGameResult({ count: gameCount });
+  });
+};
+
+const GameProcess = ({ carObject, gameCount }: Props): void => {
+  handleCarGame({ carObject, gameCount });
+
+  renderRacingGame(carObject);
+  renderGameWinner(carObject);
 };
 
 export default GameProcess;
