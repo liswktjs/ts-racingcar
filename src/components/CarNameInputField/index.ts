825750ebe519fe5859a21ba26e822ef46c261878
JASUN LEE <liswktjs@gmail.com>
2024-01-30 22:50:35 +0900

    refactor: field부분 렌더링 분리

--- src/components/CarNameInputField/index.ts
+++ src/components/CarNameInputField/index.ts
@@ -1,4 +1,4 @@
-import { DOM_ID } from '../../constant';
+import { DOM_ID, STYLE_CLASS } from '../../constant';
 import { setCarNameList } from '../../state';
 import {
   alertWrongInput,
@@ -11,14 +11,7 @@ import Button from '../@common/Button';
 import Input from '../@common/Input';
 
 const LABEL_ID = 'game-name-label';
-
-const renderLabel = (): void => {
-  const label = document.createElement('label');
-  label.id = LABEL_ID;
-  label.innerText = '5자 이하의 자동차 이름을 콤마 로 구분하여 입력해주세요';
-
-  appendChildElement({ targetElement: label, parentId: DOM_ID.gameName });
-};
+const CONTAINER_ID = 'game-name-container';
 
 const handleCarNameInput = (): void => {
   const value = getInputValue(DOM_ID.gameNameInput);
@@ -29,25 +22,44 @@ const handleCarNameInput = (): void => {
   alertWrongInput('각각의 자동차 이름은 5글자 이하여야 합니다');
 };
 
-const CarNameInputField = (): void => {
-  appendChildSection({ sectionId: DOM_ID.gameName });
-  renderLabel();
+const renderLabel = (): void => {
+  const label = document.createElement('label');
+  label.id = LABEL_ID;
+  label.className = STYLE_CLASS.label;
+
+  label.innerText = '5자 이하의 자동차 이름을 콤마 로 구분하여 입력해주세요';
+
+  appendChildElement({ targetElement: label, parentId: DOM_ID.gameName });
+};
+
+const renderGameNameInputField = (): void => {
+  const container = document.createElement('div');
+  container.className = STYLE_CLASS.labelContainer;
+  container.id = CONTAINER_ID;
+
+  appendChildElement({ parentId: LABEL_ID, targetElement: container });
 
   Input({
     id: DOM_ID.gameNameInput,
-    parentId: LABEL_ID,
+    parentId: CONTAINER_ID,
     inputType: 'text',
     placeholder: 'west',
   });
 
   Button({
     id: DOM_ID.gameNameButton,
-    parentId: LABEL_ID,
+    parentId: CONTAINER_ID,
     clickAction: () => {
       handleCarNameInput();
     },
     buttonText: '확인',
   });
+};
+
+const CarNameInputField = (): void => {
+  appendChildSection({ sectionId: DOM_ID.gameName });
+  renderLabel();
+  renderGameNameInputField();
 };
 
 export default CarNameInputField;
--- src/components/GameCountInputField/index.ts
+++ src/components/GameCountInputField/index.ts
@@ -1,4 +1,4 @@
-import { DOM_ID } from '../../constant';
+import { DOM_ID, STYLE_CLASS } from '../../constant';
 import { setGameCount } from '../../state';
 import {
   alertWrongInput,
@@ -11,14 +11,7 @@ import Button from '../@common/Button';
 import Input from '../@common/Input';
 
 const LABEL_ID = 'game-count-label';
-
-const renderLabel = (): void => {
-  const label = document.createElement('label');
-  label.id = LABEL_ID;
-  label.innerText = '시도할 횟수를 입력해주세요';
-
-  appendChildElement({ targetElement: label, parentId: DOM_ID.gameCount });
-};
+const CONTAINER_ID = 'game-count-container';
 
 const handleGameCountInput = (): void => {
   const value = getInputValue(DOM_ID.gameCountInput);
@@ -29,25 +22,44 @@ const handleGameCountInput = (): void => {
   alertWrongInput('게임 횟수는 정수로 입력해주세요');
 };
 
-const GameCountInputField = (): void => {
-  appendChildSection({ sectionId: DOM_ID.gameCount });
-  renderLabel();
+const renderLabel = (): void => {
+  const label = document.createElement('label');
+  label.id = LABEL_ID;
+  label.className = STYLE_CLASS.label;
+
+  label.innerText = '시도할 횟수를 입력해주세요';
+
+  appendChildElement({ targetElement: label, parentId: DOM_ID.gameCount });
+};
+
+const renderGameCountInputField = (): void => {
+  const container = document.createElement('div');
+  container.className = STYLE_CLASS.labelContainer;
+  container.id = CONTAINER_ID;
+
+  appendChildElement({ parentId: LABEL_ID, targetElement: container });
 
   Input({
     id: DOM_ID.gameCountInput,
-    parentId: LABEL_ID,
+    parentId: CONTAINER_ID,
     inputType: 'number',
     placeholder: '10',
   });
 
   Button({
     id: DOM_ID.gameCountButton,
-    parentId: LABEL_ID,
+    parentId: CONTAINER_ID,
     clickAction: () => {
       handleGameCountInput();
     },
     buttonText: '확인',
   });
+};
+
+const GameCountInputField = (): void => {
+  appendChildSection({ sectionId: DOM_ID.gameCount });
+  renderLabel();
+  renderGameCountInputField();
 };
 
 export default GameCountInputField;
